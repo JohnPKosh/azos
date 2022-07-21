29fedd7295d9a825156920f21e45bee771a2afcb
JohnPKosh <devprohome@live.com>
2022-07-21 08:27:27 -0400

    refactor WebShim

--- src/poc/kweb/Program.cs
+++ src/poc/kweb/Program.cs
@@ -10,11 +10,11 @@ public class Program
     //app.Run();
 
     var app1 = await BuildAndStartAppAsync(args).ConfigureAwait(false);
-    var app2 = await BuildAndStartAppAsync(args).ConfigureAwait(false);
+    //var app2 = await BuildAndStartAppAsync(args).ConfigureAwait(false);
 
     Console.ReadKey(true);
     app1.StopAsync().Wait();
-    app2.StopAsync().Wait();
+    //app2.StopAsync().Wait();
   }
 
   public async static Task<WebApplication> BuildAndStartAppAsync(string[] args)
--- src/poc/kweb/Server/WebShim.cs
+++ src/poc/kweb/Server/WebShim.cs
@@ -1,11 +1,20 @@
-﻿namespace kweb.Server;
+﻿using Microsoft.AspNetCore.Server.Kestrel.Core;
+using System.Net;
 
+namespace kweb.Server;
+
+/// <summary>
+/// The main Kestrel WebApplication catch-all logic that can bootstrap and proxy the logic to Azos.Wave
+/// </summary>
 public class WebShim
 {
+  /// <summary>
+  /// Creates and Builds a Kestral WebApplication that proxies logic through existing Azos web logic based on the supplied args and configuration
+  /// </summary>
   public WebApplication BuildWebApplication(string[] args)
   {
     var builder = getBuilder(args);
-    builder.WebHost.ConfigureKestrel(WebShimExtensions.LoadKestrelConfig()); // TODO: create options model to pass as parameter to LoadKestrelConfig
+    builder.WebHost.ConfigureKestrel(loadKestrelConfig()); // TODO: create options model to pass as parameter to LoadKestrelConfig
     var app = builder.Build();
 
     app.UseRouting();
@@ -13,12 +22,14 @@ public class WebShim
     {
       endpoints.Map("{**slug}", context =>
       {
-        // Do work here!
+        // **** TODO: Proxy RequestDelegate logic to Azos here ****
         return context.Response.WriteAsync(context.Request.QueryString.ToString());
       });
     });
     return app;
   }
+
+  #region Private Methods
 
   private WebApplicationBuilder getBuilder(string[] args)
   {
@@ -33,4 +44,32 @@ public class WebShim
     });
     return builder;
   }
+
+  private Action<KestrelServerOptions> loadKestrelConfig() // **** TODO: create options model to pass as parameter ****
+  {
+    return serverOptions =>
+    {
+      serverOptions.Limits.MaxConcurrentConnections = 1000;
+      serverOptions.Limits.MaxConcurrentUpgradedConnections = 1000;
+      serverOptions.Limits.MaxRequestBodySize = 100 * 1024 * 1024;
+      serverOptions.Limits.MinRequestBodyDataRate = new MinDataRate(bytesPerSecond: 100, gracePeriod: TimeSpan.FromSeconds(10));
+      serverOptions.Limits.MinResponseDataRate = new MinDataRate(bytesPerSecond: 100, gracePeriod: TimeSpan.FromSeconds(10));
+
+      // HTTP
+      serverOptions.Listen(IPAddress.Loopback, 5105);
+
+      // HTTPS
+      serverOptions.Listen(IPAddress.Loopback, 5106,
+          listenOptions =>
+          {
+            listenOptions.Protocols = HttpProtocols.Http2;
+            listenOptions.UseHttps("localhost.pfx", "YourSecurePassword");
+          });
+
+      serverOptions.Limits.KeepAliveTimeout = TimeSpan.FromMinutes(5);
+      serverOptions.Limits.RequestHeadersTimeout = TimeSpan.FromMinutes(1);
+    };
+  }
+
+  #endregion
 }
